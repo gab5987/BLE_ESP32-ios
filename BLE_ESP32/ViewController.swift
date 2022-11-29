a2a5b564566b0928ffd4d64ef1110a0b9eec7744
gab5987 <biell.gb4@gmail.com>
2022-11-29 20:54:38 -0300

    Feat: New segue to presentatation screen

--- BLE_ESP32/ViewController.swift
+++ BLE_ESP32/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     var simpleBluetoothIO: SimpleBluetoothIO! // Creates a BLE interface
     
+    // This method creates a gradient color for the background
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 21.0/255.0, green: 147.0/255.0, blue: 195.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 38.0/255.0, green: 28.0/255.0, blue: 71.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    // This method is called right before everything is loaded on screen, it calls the gradient method
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
+    
+    @IBOutlet weak var testeInputField: UITextField!
     @IBOutlet weak var serviceInputField: UITextField!                   // Holds the device name (Input from User)
     @IBOutlet weak var creatingAdapterActivity: UIActivityIndicatorView! // Used to create a loading animation
     @IBAction func connectToAdapter(_ sender: Any) {                     // Function that connects to the device specified in serviceInputField
@@ -31,6 +51,17 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(
             UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         )
+        // Creates a timer to fire up the segue for the initial pop up view, function declared down on "timeToMoveOn"
+        let _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
+    }
+    
+    // Simply performs a segue for the presentation view
+    @objc func timeToMoveOn() {
+        self.performSegue(withIdentifier: "initialPresentationSegue", sender: self)
+    }
+    
+    @IBAction func buttonTeste(_ sender: Any) {
+        simpleBluetoothIO.writeValue(value: testeInputField.text!)
     }
     
     func showAnim() {
@@ -44,6 +75,7 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: SimpleBluetoothIODelegate {
-    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: Int8) {
+    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: String) {
+        print(value)
     }
 }
